27d0f7f60563e88c7c152c5a6d1b662d4c7a6f92
Sean Swehla <thor@digitalocean.com>
2018-04-11 09:20:02 -0400

    Draw circles in a circle

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,23 @@
+var r;
+var n;
+
 function setup() {
 	createCanvas(640, 480);
+	r = height * 0.45;
+	n = 10;
 }
 
 function draw() {
-	if(mouseIsPressed) {
-		fill(0);
-	} else {
-		fill(255);
+	var count;
+	background(0);
+	translate(width/2, height/2);
+	ellipseMode(CENTER);
+	noStroke();
+	fill(200);
+	for(count = 0; count < n; count++) {
+		theta = ((2 * Math.PI) / n) * count;
+		var x = r * cos(theta);
+		var y = r * sin(theta);
+		ellipse(x, y, 32, 32);
 	}
-	ellipse(mouseX, mouseY, 80, 80);
 }
