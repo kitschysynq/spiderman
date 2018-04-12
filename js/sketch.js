96016ea35b182ca88c79be92326b531d94c52e34
Sean Swehla <thor@digitalocean.com>
2018-04-11 20:16:48 -0400

    Add slider and tie color to its value

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,16 @@
 var r;
 var n;
 var minorR;
+var slider;
 
 function setup() {
 	createCanvas(640, 480);
 	r = height * 0.45;
 	n = 10;
 	minorR = ((2 * Math.PI * r) / n) * 0.8;
+
+	slider = createSlider(0, 360, 0);
+	slider.position(20, 20);
 }
 
 function draw() {
@@ -20,6 +24,8 @@ function draw() {
 		theta = ((2 * Math.PI) / n) * count;
 		var x = r * cos(theta);
 		var y = r * sin(theta);
+		var color = (Math.tanh((theta - slider.value()) / minorR) + 1) * 128;
+		fill(color);
 		ellipse(x, y, minorR, minorR);
 	}
 }
